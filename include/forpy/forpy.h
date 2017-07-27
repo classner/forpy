b5f8a7f072afa643bb443c80c08608bd10721d62
Christoph Lassner <mail@christophlassner.de>
2017-07-27 02:57:33 +0200

    Added missing file.

---
+++ include/forpy/forpy.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef FORPY_FORPY_H_
+#define FORPY_FORPY_H_
+
+#include "./global.h"
+#include "./types.h"
+
+#include "./impurities/impurities.h"
+#include "./gains/gains.h"
+#include "./leafs/leafs.h"
+#include "./data_providers/data_providers.h"
+#include "./features/features.h"
+#include "./deciders/deciders.h"
+
+#endif  // FORPY_FORPY_H_
